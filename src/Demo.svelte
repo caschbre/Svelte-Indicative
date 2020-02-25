5a808b48241fe07ae6e0618b00959a021593d93d
Craig Aschbrenner <caschbre+bitbucket@gmail.com>
2020-02-25 12:34:57 -0600

    WIP: Moving to monorepo.

--- src/Demo.svelte
+++ src/Demo.svelte
@@ -1,14 +1,132 @@
 <script>
-  import { indicative } from './components/indicative.js';
+  // @todo ability to read field params (e.g. required, minlength, etc.)
+  import { svelteIndicative } from './components/indicative.js';
+
+  const { state, isValid, validator } = svelteIndicative();
+
+  let indicativeParams = {
+    validationSchema: {
+      textfield: 'required|alpha|min:4',
+      singlecheckbox: 'required',
+      multiplecheckboxes: 'required',
+      radios: 'required',
+      singleselect: 'required',
+      multipleselect: 'required'
+    },
+    messages: {
+      required: (field, validation, args) => {
+        return `${field} is required.`;
+      }
+    },
+    // @todo
+    // sanitizerSchema: {}
+  };
+
+  let data = {
+    textfield: '',
+    checkbox: false,
+    multiplecheckboxes: [],
+    radios: '',
+  };
+
+  $: isFormValid = isValid($state);
+  $: isFieldValid = (field) => isValid($state, field);
+  $: isValidating = $state.isValidating;
+
+  const onSubmit = (evt) => {
+    console.log('onSubmit', evt);
+  };
+
+  const onValidated = (results) => {
+    console.log('onValidated', results);
+  };
 </script>
 
-<form use:indicative={{}}>
-  <label>Text Field</label>
-  <input type="text" name="textfield" />
+<form
+  id="theform"
+  use:validator={indicativeParams}
+  on:submit|preventDefault={onSubmit}
+  on:validated={onValidated}
+  class:form-invalid={!isFormValid}>
 
-  <button type="submit">Submit</button>
+  <div class="form-field">
+    <label>Text Field</label>
+    <input id="textfield" type="text" name="textfield"
+    class:invalid={!isFieldValid('textfield')} />
+  </div>
+
+  <label class="form-field" class:invalid={!isFieldValid('singlecheckbox')}>
+    <input type="checkbox" name="singlecheckbox" value="single checkbox">
+    <span>Single Checkbox</span>
+  </label>
+
+  <div class="form-field">
+    <label>Multiple Checkboxes</label>
+    <label class="form-item">
+      <input type="checkbox" name="multiplecheckboxes" value="checkbox1">
+      <span>Checkbox 1</span>
+    </label>
+    <label class="form-item">
+      <input type="checkbox" name="multiplecheckboxes" value="checkbox2">
+      <span>Checkbox 2</span>
+    </label>
+    <label class="form-item">
+      <input type="checkbox" name="multiplecheckboxes" value="checkbox3">
+      <span>Checkbox 3</span>
+    </label>
+  </div>
+
+  <div class="form-field">
+    <label>Radios</label>
+    <label class="form-item">
+      <input type="radio" name="radios" value="radio1">
+      <span>Radio 1</span>
+    </label>
+    <label class="form-item">
+      <input type="radio" name="radios" value="radio2">
+      <span>Radio 2</span>
+    </label>
+    <label class="form-item">
+      <input type="radio" name="radios" value="radio3">
+      <span>Radio 3</span>
+    </label>
+  </div>
+
+  <div class="form-field">
+    <label>Single Select</label>
+    <select name="singleselect">
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+    </select>
+  </div>
+
+  <div class="form-field">
+    <label>Multiple Select</label>
+    <select id="multipleselect" name="multipleselect" multiple>
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+    </select>
+  </div>
+
+  <div class="form-field">state: {JSON.stringify($state, null, 2)}</div>
+
+  <input type="submit" value="input submit">
+  <button type="submit">Submit Button</button>
+  <input type="reset" value="input reset">
 </form>
 
 <style>
+  form {
+    padding: 0 20px;
+  }
 
+  .form-field {
+    margin: 20px 0;
+  }
+
+  .form-item {
+    display: block;
+  }
 </style>
--- src/components/indicative.js
+++ src/components/indicative.js
@@ -1,63 +1,143 @@
-export function indicative(node, params) {
-  console.log('indicative params', params);
+// @todo add sanitizer.
+// @todo option to validate on load. Useful for disabling submit button.
 
-  const handleBlur = (evt) => {
-    console.log('handleBlur', evt);
+import { get, writable, derived } from 'svelte/store';
+import { validate } from 'indicative/validator';
+
+const svelteIndicative = () => {
+  const dirtyStore = writable({});
+  const errorsStore = writable({});
+  const isValidatingStore = writable(false);
+
+  const reset = () => {
+    dirtyStore.set({});
+    errorsStore.set({});
+    isValidatingStore.set(false);
   };
 
-  const handleChange = (evt) => {
-    console.log('handleChange', evt);
+  const isDirty = (state, field) => {
+    return !field ? !!+Object.values(state.dirty).filter(value => !!+value).length : state.dirty[field] || false;
   };
 
-  const handleFocus = (evt) => {
-    console.log('handleFocus', evt);
+  const isValid = (state, field) => {
+    if (!isDirty(state, field)) {
+      // @todo return null(?) if not dirty to indicate not validated yet?
+      return true;
+    }
+
+    return !field ? !+Object.values(state.errors).filter(value => !!+value).length : !state.errors[field] || false;
   };
 
-  const handleInput = (evt) => {
-    console.log('handleInput', evt);
-  };
+  // @todo combine params with default configuration.
+  const validator = (formNode, params) => {
+    // Keep track of the fields to validate. All other fields will be ignored.
+    // @todo how to handle dynamic fields / values that exist within the
+    // provided schema?
+    const fieldNodes = getFieldNodes(params.validationSchema);
+    const fieldList = Object.keys(fieldNodes);
 
-  const handleInvalid = (evt) => {
-    console.log('handleInvalid', evt);
-  };
+    formNode.addEventListener('change', handleChange);
+    formNode.addEventListener('reset', handleReset);
+    formNode.addEventListener('submit', handleSubmit);
 
-  const handleReset = (evt) => {
-    console.log('handleReset', evt);
-  };
+    // This fires on the form element when a child element changes so we can
+    // handle individual field validation.
+    function handleChange(evt) {
+      const { name: field, value } = event.target;
+      setDirty(field, true);
+      doValidate({ [field]: value }, params);
+    }
 
-  const handleSubmit = (evt) => {
-    console.log('handleSubmit', evt);
-  };
+    function handleReset(evt) {
+      reset();
+    }
 
-  const handleUpdate = (evt) => {
-    console.log('handleUpdate', evt);
-  };
+    function handleSubmit(evt) {
+      evt.preventDefault();
 
-  node.addEventListener('onblur', handleBlur);
-  node.addEventListener('onchange', handleChange);
-  node.addEventListener('onfocus', handleFocus);
-  node.addEventListener('oninput', handleInput);
-  node.addEventListener('oninvalid', handleInvalid);
-  node.addEventListener('onreset', handleReset);
-  node.addEventListener('onsubmit', handleSubmit);
-  node.addEventListener('onupdate', handleUpdate);
+      // @todo after validating...
+      formNode.dispatchEvent(
+        new CustomEvent('validated', {
+          bubbles: true,
+          cancelable: true,
+          detail: {
+            todo: '@todo what results to provide?',
+            params: params,
+          }
+        })
+      );
+    }
+
+    async function doValidate(data, params) {
+      // messages = messages || {};
+      // @todo include cacheKey for performance improvements.
+      // @todo include removeAdditional option?
+      // options = options || {};
+
+      isValidatingStore.set(true);
+
+      // @todo validate / validateAll.
+      await validate(data, params.validationSchema, params.messages, params.options)
+        // @todo clear errors for field or all errors.
+        .then(console.log)
+        .catch(errors => {
+          console.warn(errors);
+        })
+        .finally(() => {
+          isValidatingStore.set(false);
+        });
+    }
+
+    function setDirty(field, value) {
+      dirtyStore.update(current => {
+        current[field] = value;
+        return current;
+      });
+    };
+
+    function setError(field, values) {
+      errorsStore.update(current => {
+        current[field] = values;
+        return current;
+      });
+    };
+
+    function clearError(field) {
+      errorsStore.update(current => {
+        delete current[field];
+        return current;
+      });
+    }
+
+    function getFieldNodes(validationSchema) {
+      return Object.keys(validationSchema).reduce((acc, field) => {
+        acc[field] = formNode.elements[field] || null;
+        return acc;
+      }, {});
+    }
+
+    return {
+      update(params) {
+        // @todo
+      },
+      destroy() {
+        formNode.removeEventListener('change', handleChange);
+        formNode.removeEventListener('reset', handleReset);
+        formNode.removeEventListener('submit', handleSubmit);
+      }
+    };
+  }
 
   return {
-    update(params) {
-      // @todo
-      console.log('indicative update params', params);
-    },
-    destroy() {
-      console.log('indicative destroy');
-
-      node.removeEventListener('onblur', handleBlur);
-      node.removeEventListener('onchange', handleChange);
-      node.removeEventListener('onfocus', handleFocus);
-      node.removeEventListener('oninput', handleInput);
-      node.removeEventListener('oninvalid', handleInvalid);
-      node.removeEventListener('onreset', handleReset);
-      node.removeEventListener('onsubmit', handleSubmit);
-      node.removeEventListener('onupdate', handleUpdate);
-    }
+    state: derived([dirtyStore, errorsStore, isValidatingStore], ([$dirtyStore, $errorsStore, $isValidatingStore]) => ({
+      dirty: $dirtyStore,
+      errors: $errorsStore,
+      isValidating: $isValidatingStore
+    })),
+    isValid: isValid,
+    reset,
+    validator
   };
-}
+};
+
+export { svelteIndicative };
